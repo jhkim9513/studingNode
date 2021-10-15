00453a91ad36b5ebacc62d6d358f0308f5f4d655
김종환 <dondon9513@gmail.com>
2021-10-15 17:06:35 +0900

    try using npm

--- node/13-event/logger.js
+++ node/13-event/logger.js
@@ -1,4 +1,6 @@
 const EventEmitter = require("events");
+// emitter에서 등록한것은 다른 emitter에서 들을 수 없다.
+// 때문에 class로 만들어 exports해야만 재사용할 수 있다.
 
 class Logger extends EventEmitter {
   // EventEmitter 를 상속했기에 emit을 바로 사용할 수 있다.
