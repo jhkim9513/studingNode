2acba5bb7a77affdeb52df64f01dc46ba51fea22
김종환 <dondon9513@gmail.com>
2021-10-15 17:29:11 +0900

    npm basic

--- npm/app.js
+++ npm/app.js
@@ -1,1 +1,21 @@
 console.log("hello");
+
+//npm i -h
+
+//npm list -g
+//global하게 설치한것 보기
+
+//npm list --dept=0
+//내가 설치한것만 보기
+
+//version
+/*
+  버전 x.y.z에서 x는 major, y는 minor, z는 patch로 
+  자잘한 버그수정은 patch, 간단한 기능 추가는 minor이다.
+  ^는 major고정, ~는 minor까지 고정이다 예를들면
+  ^4.3.0 는 major 가 4인 모든 버전을 포함하는것이고
+  ~4.3.0 는 4.3까지 고정이고 patch는 모두 포함하는것이다.
+*/
+
+//삭제
+// npm uninstall 이름  또는 npm un 이름
