b3a52f9dbcd78e676cd864569336f4c077ca4cfb
jhkim9513 <dondon9513@gmail.com>
2021-11-11 13:56:03 +0900

    made a file cleanup script

--- photo/app.js
+++ photo/app.js
@@ -30,14 +30,39 @@ fs.promises.readdir(workingDir)//
 function processFiles(files) {
   files.forEach((file) => {
     if(isVideoFile(file)) {
-      console.log(`video : ${file}`)
+      move(file, videoDir)
     } else if(isCapturedFile(file)) {
-      console.log(`captured : ${file}`)
-    } else if(isDuplicatedFile(file)) {
-      console.log(`duplicated : ${file}`)
+      move(file, captureDir)
+    } else if(isDuplicatedFile(files, file)) {
+      move(file, duplicatedDir)
     }
   })
 }
 
-// 7.3 5:55
-// function isVideoFile
+function isVideoFile(file) {
+  const regExp = /(mp4|mov)$/gm;
+  const match = file.match(regExp);
+  return !!match;
+}
+function isCapturedFile(file) {
+  const regExp = /(png|aae)$/gm;
+  const match = file.match(regExp);
+  return !!match;
+}
+function isDuplicatedFile(files, file) {
+  // IMG_XXXX -> IMG_EXXX
+  if(!file.startsWith('IMG_') || file.startsWith('IMG_E')) {
+    return false;
+  }
+
+  const edited = `IMG_E${file.split('_')[1]}`;
+  const found = files.find(f => f.includes(edited))
+  return !!found;
+}
+function move(file, targetDir) {
+  console.info(`move ${file} to ${path.basename(targetDir)}`)
+  const oldPath = path.join(workingDir, file);
+  const newPath = path.join(targetDir, file);
+  fs.promises.rename(oldPath, newPath)
+  .catch(console.error);
+}
